500372c8dbf5de10f87a60ab9bd50d666b1590b3
Sammy Kerata Oina <44265300+SammyOina@users.noreply.github.com>
2020-09-22 14:02:16 +0300

    modularized

--- rpi.py
+++ rpi.py
@@ -8,6 +8,8 @@ import random
 import jwt
 import paho.mqtt.client as mqtt
 
+from deps import *
+
 # GCP parameters 
 project_id = 'qualis-c1bec'  # Your project ID.
 registry_id = 'speed-registry'  # Your registry name.
@@ -20,71 +22,6 @@ mqtt_bridge_hostname = 'mqtt.googleapis.com'  # GC bridge hostname.
 mqtt_bridge_port = 8883  # Bridge port.
 message_type = 'event'  # Message type (event or state).
 
-def create_jwt(project_id, private_key_file, algorithm):
-    # Create a JWT (https://jwt.io) to establish an MQTT connection.
-    token = {
-        'iat': datetime.datetime.utcnow(),
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-        'aud': project_id
-    }
-    with open(private_key_file, 'r') as f:
-        private_key = f.read()
-    print('Creating JWT using {} from private key file {}'.format(
-        algorithm, private_key_file))
-    return jwt.encode(token, private_key, algorithm=algorithm)
-
-
-def error_str(rc):
-    # Convert a Paho error to a human readable string.
-    return '{}: {}'.format(rc, mqtt.error_string(rc))
-
-
-class Device(object):
-    # Device implementation.
-    def __init__(self):
-        self.connected = False
-    
-    def wait_for_connection(self, timeout):
-        # Wait for the device to become connected.
-        total_time = 0
-        while not self.connected and total_time < timeout:
-            time.sleep(1)
-            total_time += 1
-
-        if not self.connected:
-            raise RuntimeError('Could not connect to MQTT bridge.')
-
-    def on_connect(self, unused_client, unused_userdata, unused_flags, rc):
-        # Callback on connection.
-        print('Connection Result:', error_str(rc))
-        self.connected = True
-
-    def on_disconnect(self, unused_client, unused_userdata, rc):
-        # Callback on disconnect.
-        print('Disconnected:', error_str(rc))
-        self.connected = False
-
-    def on_publish(self, unused_client, unused_userdata, unused_mid):
-        # Callback on PUBACK from the MQTT bridge.
-        print('Published message acked.')
-
-    def on_subscribe(self, unused_client, unused_userdata, unused_mid,
-                     granted_qos):
-        # Callback on SUBACK from the MQTT bridge.
-        print('Subscribed: ', granted_qos)
-        if granted_qos[0] == 128:
-            print('Subscription failed.')
-
-    def on_message(self, unused_client, unused_userdata, message):
-        # Callback on a subscription.
-        payload = message.payload.decode('utf-8')
-        print('Received message \'{}\' on topic \'{}\' with Qos {}'.format(payload, message.topic, str(message.qos)))
-        
-        if not payload:
-            return
-        # Parse incoming JSON.
-        data = json.loads(payload)
-        
            
 def main():
 
@@ -130,6 +67,7 @@ def main():
                 # Form payload in JSON format.
                 data = {
                     'num_message' : num_message,
+                    'vehicle_Id': 1234,
                     'speed': speed,
                     'message': "Hello",
                     'time' : currentTime

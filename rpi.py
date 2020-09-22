c9b903d1b336cc14f228f765a0bb634f54f4a34f
Sammy Kerata Oina <44265300+SammyOina@users.noreply.github.com>
2020-09-22 13:21:22 +0300

    do not ack

--- rpi.py
+++ rpi.py
@@ -107,9 +107,9 @@ def main():
     client.on_connect = device.on_connect
     client.on_publish = device.on_publish
     client.on_disconnect = device.on_disconnect
-    client.on_subscribe = device.on_subscribe
-    client.on_message = device.on_message
-    client.connect(mqtt_bridge_hostname, mqtt_bridge_port)
+    #client.on_subscribe = device.on_subscribe
+    #client.on_message = device.on_message
+    #client.connect(mqtt_bridge_hostname, mqtt_bridge_port)
     client.loop_start()
 
     mqtt_telemetry_topic = '/devices/{}/events'.format(device_id)
@@ -122,7 +122,7 @@ def main():
     
     num_message = 0
     try:        
-        while True:
+        while num_message <= 10:
             speed = random.randint(0,80)
             if True:   
                 currentTime = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
